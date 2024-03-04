e48705bd90667945b8ad4de7be8cb9465785394e
Tracy Zhou <TracyZ14@nycstudents.net>
2024-03-04 14:07:32 -0500

    Completed the findCorrectPath method, fixed the findNextCoordinate and isPreviousCoordinate and printCorrectPath methods, and deleted the isDeadEnd method

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -2,49 +2,34 @@ import java.util.ArrayList;
 public class MazeSolver
 {
     private String[][] maze;
-    private ArrayList<ArrayList<String>> possiblePaths;
-    private ArrayList<String> correctPath;
+    private ArrayList<ArrayList<int[]>> possiblePaths;
+    private ArrayList<int[]> correctPath;
     private int endingPointRow;
     private int endingPointColumn;
 
     public MazeSolver(String[][] maze)
     {
         this.maze = maze;
-        this.possiblePaths = new ArrayList<ArrayList<String>>();
-        this.correctPath = new ArrayList<String>();
+        this.possiblePaths = new ArrayList<ArrayList<int[]>>();
+        this.correctPath = new ArrayList<int[]>();
         this.endingPointRow = maze.length - 1;
         this.endingPointColumn = maze[0].length - 1;
     }
 
-    public ArrayList<int[]> findNextCoordinate(int row, int column)
+    public ArrayList<int[]> findNextCoordinate(int[] currentCoordinate)
     {
+        int row = currentCoordinate[0];
+        int column = currentCoordinate[1];
+        System.out.println(row + " + " + column);
         ArrayList<int[]> nextCoordinates = new ArrayList<int[]>();
         int[] coordinate = new int[2];
         if(row != 0)
         {
-            if(column != 0)
-            {
-                if(maze[row - 1][column - 1].equals("."))
-                {
-                    coordinate[0] = row - 1;
-                    coordinate[1] = column - 1;
-                    nextCoordinates.add(coordinate);
-                }
-            }
             if(maze[row - 1][column].equals("."))
             {
                 coordinate[0] = row - 1;
                 coordinate[1] = column;
                 nextCoordinates.add(coordinate);
-            }
-            if(column != endingPointColumn)
-            {
-                if(maze[row - 1][column + 1].equals("."))
-                {
-                    coordinate[0] = row - 1;
-                    coordinate[1] = column + 1;
-                    nextCoordinates.add(coordinate);
-                }
             }
         }
         if(column != 0)
@@ -67,68 +52,65 @@ public class MazeSolver
         }
         if(row != endingPointRow)
         {
-            if(column != 0)
-            {
-                if(maze[row = 1][column - 1].equals("."))
-                {
-                    coordinate[0] = row + 1;
-                    coordinate[1] = column - 1;
-                    nextCoordinates.add(coordinate);
-                }
-            }
             if(maze[row + 1][column].equals("."))
             {
                 coordinate[0] = row + 1;
                 coordinate[1] = column;
                 nextCoordinates.add(coordinate);
             }
-            if(column != endingPointColumn)
-            {
-                if(maze[row + 1][column + 1].equals("."))
-                {
-                    coordinate[0] = row + 1;
-                    coordinate[1] = column + 1;
-                    nextCoordinates.add(coordinate);
-                }
-            }
         }
         return nextCoordinates;
     }
 
-    public boolean isPreviousCoordinate(int previousRow, int previousColumn, int row, int column)
+    public boolean isPreviousCoordinate(int[] previousCoordinate, int[] coordinate)
     {
-        if((row == previousRow) && (column == previousColumn))
+        if(coordinate.equals(previousCoordinate))
         {
             return true;
         }
         return false;
-    }
-
-    public boolean isDeadEnd(int previousRow, int previousColumn, int row, int column)
-    {
-        ArrayList<int[]> nextCoordinates = findNextCoordinate(row, column);
-        int[] previousCoordinates = {previousRow, previousColumn};
-        if((nextCoordinates.size() == 1) && (nextCoordinates.get(1).equals(previousCoordinates)))
-        {
-            return true;
-        }
-        return false;
-    }
-
-    public void addCoordinate(ArrayList<String> path, int row, int column)
-    {
-        path.add("(" + row + ", " + column + ")");
     }
 
     public String findCorrectPath()
     {
         int[] endingCoordinate = {endingPointRow, endingPointColumn};
-        int[] nextCoordinate = new int[2];
-        ArrayList<String> path = new ArrayList<String>();
+        int[] coordinate = {0, 0};
+        ArrayList<int[]> path = new ArrayList<int[]>();
         possiblePaths.add(path);
-        for(int i = 0; !nextCoordinate.equals(endingCoordinate); i++)
+        path.add(coordinate);
+        while(!coordinate.equals(endingCoordinate))
         {
-            
+            ArrayList<int[]> thisPath = possiblePaths.get(0);
+            int[] previousCoordinate = thisPath.get(thisPath.size() - 1);
+            ArrayList<int[]> nextCoordinates = findNextCoordinate(previousCoordinate);
+            for(int[] coordinates : nextCoordinates)
+            {
+                System.out.println(coordinates[0] + ", " + coordinates[1]);
+            }
+            System.out.println();
+            for(int i = nextCoordinates.size() - 1; i >= 0; i--)
+            {
+                if(isPreviousCoordinate(previousCoordinate, nextCoordinates.get(i)))
+                {
+                    nextCoordinates.remove(i);
+                }
+            }
+            if(nextCoordinates.size() == 1)
+            {
+                thisPath.add(nextCoordinates.get(0));
+            }
+            else
+            {
+                for(int[] coordinates : nextCoordinates)
+                {
+                    ArrayList<int[]> newPath = thisPath;
+                    thisPath.add(coordinates);
+                    possiblePaths.add(newPath);
+                }
+                possiblePaths.remove(0);
+                thisPath = possiblePaths.get(0);
+            }
+            coordinate = thisPath.get(thisPath.size() - 1);
         }
         return printCorrectPath();
     }
@@ -136,10 +118,11 @@ public class MazeSolver
     public String printCorrectPath()
     {
         String path = "";
-        for(String coordinate : correctPath)
+        int[] endingCoordinate = {endingPointRow, endingPointColumn};
+        for(int[] coordinate : correctPath)
         {
-            path += coordinate;
-            if(coordinate != ("(" + endingPointRow + ", " + endingPointColumn + ")"))
+            path += ("(" + coordinate[0] + ", " + coordinate[1] + ")");
+            if(!coordinate.equals(endingCoordinate))
             {
                 path += " ---> ";
             }
